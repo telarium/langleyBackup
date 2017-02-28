b9337298d9db5447ef8fb2ea40085c41622f9adf
telarium <andrew@langleycreations.com>
2017-02-28 22:06:59 +0000

    Added Samba and Mega Upload setup

--- langleyBackup.py
+++ langleyBackup.py
@@ -11,15 +11,17 @@ class LangleyBackup():
 		if not os.path.exists('/mnt/usb1/'):
         		os.makedirs('/mnt/usb1/')
 
-		subprocess.call("sudo mount /dev/sda1 /mnt/usb1 > /dev/null 2>&1", shell=True)
+		subprocess.call('sudo mount /dev/sda1 /mnt/usb1 > /dev/null 2>&1', shell=True)
+		subprocess.Popen('sudo /etc/init.d/samba restart', shell=True,stdout=open(os.devnull, 'w'), stderr=subprocess.STDOUT)
 		self.megaUsername = None
 		self.getLocalDirectoryList()
 		self.getRemoteDirectoryList()
 
 	def getMegaCredentials(self):
+		print "Logging into Mega Upload..."
 		lines = open(PATH+'/megaLogin.txt','r').read().split('\n')
-		self.megaUsername = lines[0]
-		self.megaPassword = lines[1]
+		self.megaUsername = lines[0].rstrip()
+		self.megaPassword = lines[1].rstrip()
 
 	def ignoreDirectory(self,dir):
 		if dir.find('/.') != -1:
@@ -31,8 +33,9 @@ class LangleyBackup():
 
 	def getLocalDirectoryList(self):
 		statvfs = os.statvfs('/mnt/usb1/')
+		label = subprocess.check_output(['sudo','blkid','-o','value','-s','LABEL','/dev/sdb1']).rstrip()
 		f = open(PATH+'/fileList.txt', 'w')
-		f.write(str(statvfs.f_frsize * statvfs.f_bfree)+'\n')
+		f.write(str(statvfs.f_frsize * statvfs.f_bfree)+'\n'+label+'\n')
 		f.close()
 		f = open(PATH+'/fileList.txt', 'a')
 		for x in os.walk('/mnt/usb1'):
@@ -45,6 +48,7 @@ class LangleyBackup():
 		if self.megaUsername == None:
 			self.getMegaCredentials()
 
+		print "Fetching Mega Upload file list..."
 		subprocess.call(['megals', '-u', self.megaUsername, '-p', self.megaPassword])
 
 backup = LangleyBackup()
--- setup.sh
+++ setup.sh
@@ -6,7 +6,11 @@ sudo apt-get -y install build-essential libglib2.0-dev \
 	libssl-dev libcurl4-openssl-dev libgirepository1.0-dev \
 	exfat-fuse exfat-utils ntfs-3g \
 	autoconf libgtk2.0-dev libssl-dev \
-	libcurl4-openssl-dev asciidoc git
+	libcurl4-openssl-dev asciidoc git \
+	samba samba-common-bin
+
+printf "[Backup Drive 1]\ncomment = Backup Drive 1\npath = /mnt/usb1/\nbrowseable = yes\nread only = no\nguest ok = yes\ncreate mask = 0660\ndirectory mask = 2770\n" | sudo tee /etc/samba/smb.conf -a
+sudo smbpasswd -a root
 
 cd ~
 wget https://github.com/libfuse/libfuse/releases/download/fuse-3.0.0pre0/fuse-3.0.0pre0.tar.gz
@@ -24,3 +28,12 @@ autoreconf
 ./configure
 make
 sudo make install
+
+echo -n "Enter your Mega Upload account e-mail: "
+read email
+echo -n "Enter your Mega Upload account password: "
+read pwd
+
+printf "$email\n$pwd" | sudo tee megaLogin.txt -a
+
+
